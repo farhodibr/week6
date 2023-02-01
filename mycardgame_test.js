7d10e12aaf303c0a1363b5eb5cdde21afee071bc
farhodibr <96195070+farhodibr@users.noreply.github.com>
2023-02-01 16:58:52 -0500

    Add files via upload

--- mycardgame_test.js
+++ mycardgame_test.js
@@ -2,21 +2,14 @@ var expect = chai.expect;
 
 describe('My functions', function() {
     describe('#createDeck()', function() {
-        it('should create an array with a length of 52 indexes ', function() {
-           
-            var x = new Deck;
-            var y = x.createDeck('a', 'b');
-            var i = x.cards;
-            expect(i).to.be.an('array'); // checking x.cards if array created;
-            expect(i).to.have.a.lengthOf(52); // checking if x.cards has a length of 52;
-            expect(x).to.be.an('object');  // checking if new Deck is an object;
-
+        it('should create deck', function() {
+            var x = createDeck('x', 'y');
+            expect(x).to.equal(Array);
         });
 
         it('should throw an error if parametr is not an array', function() {
             expect(function() {
-                
-                Deck('a', 'b');
+                createDeck(); 
 
             }).to.throw(Error);
         })
